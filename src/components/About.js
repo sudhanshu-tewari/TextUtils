f49e95ce7a293f0a30aa1dece603bd1741d95056
sudhanshu-tewari <39265139+sudhanshu-tewari@users.noreply.github.com>
2022-12-15 16:24:03 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About(props) {
          <>
           <div className="container my-3">
                  <h1>About Us</h1>
-                 <p>Work In Progress</p>
+                 <p>This is the Text Analyze Tool where you can convert text to upper , lowercase , copy , remove extra space , Reverse the text. This app is madein React. Further many feature will come.</p>
           </div>
                
          </>
